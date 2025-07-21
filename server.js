2c6d83d1e829c9482bd8f3f4702cc8ff16e28952
Thales Anderson <thalesanderson33@gmail.com>
2025-07-20 21:32:13 -0300

    Env no DB

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
+require('dotenv').config();
+
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 const PORT = 3000;
 
-const MONGO_URI = 'mongodb+srv://visiontech:Visiontech1207%40@cluster0.jct8w9r.mongodb.net/visiontech?retryWrites=true&w=majority&appName=Cluster0';
+const MONGO_URI = process.env.MONGO_URI;
 
 mongoose.connect(MONGO_URI, {
     useNewUrlParser: true,

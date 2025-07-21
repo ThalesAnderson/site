cac004690ce02221a73a46a84971a8d51a02f36b
Thales Anderson <thalesanderson33@gmail.com>
2025-07-20 21:53:09 -0300

    Tetse

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const MONGO_URI = process.env.MONGO_URI;
 
